ece9454f4053ea2d014249936303ad08cb232fc7
laeious <44135105+laeious@users.noreply.github.com>
2019-04-27 15:54:45 +0700

    remove comment

--- haltlex.py
+++ haltlex.py
@@ -1,20 +1,15 @@
-# An implementation of Dartmouth BASIC (1964)
-
 from ply import *
 import ply.lex as lex
 
 keywords = (
-    # 'LET', 'READ', 'DATA', 'PRINT', 'GOTO', 'THEN', 'FOR', 'NEXT', 'TO', 'STEP',
-    # 'END', 'GOSUB', 'DIM', 'REM', 'RUN', 'LIST', 'NEW',
 
     'DEF', 'VAR', 'IF', 'SHOW', 'SHOWLN', 'LOOP', 
     'LE_OP', 'GE_OP', 'EQ_OP','LT_OP', 'GT_OP','MOD_OP', 'L_BRACKET', 'R_BRACKET', 'L_CURLYBRACKET', 
     'R_CURLYBRACKET',  'ASSIGN_OP', 
     'ADD_OP', 'MINUS_OP', 'MUL_OP', 'DIVIDE_OP',  'NUMBER', 
     'HEX_NUM','L_SBRACKET', 'R_SBRACKET', 'QUEST','COMMA', 'ID','EXIT','RETURN', 'EOL', 'INF','STRING'
-    # , 'HEX_NUMBER',, 'NEWLINE', 'SIGN_NUMBER' ,  'L_ANGLEBRACKET', 'R_ANGLEBRACKET', 'SEMI'  ,
-
 )
+
 tokens = keywords 
 
 t_ignore = ' \t\v\f'
@@ -38,25 +33,7 @@ def t_ID(t):
         t.type = t.value
     return t
 
-# t_PLUS = r'\+'
-# t_MINUS = r'-'
-# t_DIVIDE = r'/'
-# t_LT = r'<'
-# t_LE = r'<='
-# t_GT = r'>'
-# t_GE = r'>='
-# t_INTEGER = r'\d+'
-
-# t_EQUALS = r'='
-# t_TIMES = r'\*'
-# t_POWER = r'\^'
-# t_LPAREN = r'\('
-# t_RPAREN = r'\)'
-# t_NE = r'<>'
 t_COMMA = r'\,'
-# t_SEMI = r';'
-# t_FLOAT = r'((\d*\.\d+)(E[\+-]?\d+)?|([1-9]\d*E[\+-]?\d+))'
-
 t_LE_OP = r'<='
 t_GE_OP = r'>='
 t_LT_OP = r'<'
@@ -67,14 +44,9 @@ t_R_BRACKET = r'\)'
 t_L_BRACKET = r'\('
 t_L_CURLYBRACKET = r'\{'
 t_R_CURLYBRACKET = r'\}'
-# t_L_ANGLEBRACKET = r'\<'
-# t_R_ANGLEBRACKET = r'\>'
 t_L_SBRACKET = r'\['
 t_R_SBRACKET = r'\]'
 t_ASSIGN_OP = r'<-'
-# t_NUMBER = r'\d+'
-# t_SIGN_NUMBER = r'[-]?[0-9]+'
-# t_HEX_NUMBER = r'(Hx)?[a-fA-F0-9]+'
 t_ADD_OP = r'\+'
 t_MINUS_OP = r'-'
 t_MUL_OP = r'\*'
