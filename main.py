96f9637c8a58647106a72b59dc309d826221c480
AKEWa01 <38839186+AKEWa01@users.noreply.github.com>
2019-05-04 02:05:46 +0700

    asd

---
+++ main.py
@@ -0,0 +1,14 @@
+import parse2
+import haltASM
+import platform
+
+haltASM.main(parse2.getTree())
+file = open("test.asm", 'w')
+file.writelines(haltASM.asmheader)
+file.writelines(haltASM.asmdata)
+file.writelines(haltASM.asmtext)
+file.writelines(haltASM.asmleave)
+file.close()
+# print(parse2.getTree())
+#print(tt.asmtext)
+# print("--- compile successful ---")
--- test.bat
+++ test.bat
@@ -1,4 +1,4 @@
-python ak.py
+python main.py
 nasm -f win64 test.asm
 gcc test.obj -o test.exe
 test.exe
